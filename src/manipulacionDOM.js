be4436653abd05a9f417fbf48cf74680f7a38cc4
Mario Schmoker <103613451+MarioSchmoker@users.noreply.github.com>
2022-08-29 00:01:40 -0300

    Initial commit

---
+++ src/manipulacionDOM.js
@@ -0,0 +1,67 @@
+let body = document.querySelector("body");
+// let imagen = document.querySelector("img");
+// imagen.src =
+//   "https://loscormoranes.com/wp-content/uploads/2018/07/tierradelfuego.jpg";
+let boton = document.createElement("button");
+// ocultarDiv();
+
+// function ocultarDiv() {
+//   boton.appendChild(document.createTextNode("Ocultar/Mostar Imagen"));
+//   body.appendChild(boton);
+//   boton.addEventListener("click", () => {
+//     imagen.classList.toggle("alternar_img");
+//   });
+// }
+
+let caja = document.createElement("div");
+body.appendChild(caja);
+let lista = document.createElement("ul");
+let elemento = document.createElement("li");
+let texto = document.createTextNode("Tarea 1");
+elemento.appendChild(texto);
+lista.appendChild(elemento);
+caja.appendChild(lista);
+
+elemento = document.createElement("li");
+texto = document.createTextNode("Tarea 2");
+elemento.appendChild(texto);
+lista.appendChild(elemento);
+
+// elemento = document.createElement("li");
+// texto = document.createTextNode("Tarea 3");
+// elemento.appendChild(texto);
+// lista.appendChild(elemento);
+let nuevaTarea = String;
+nuevaTarea = document.createElement("input");
+body.appendChild(nuevaTarea);
+nuevaTarea.appendChild(document.createTextNode("Nueva Tarea"));
+// nuevaTarea = document.getElementById(this.value);
+
+boton = document.createElement("button");
+boton.appendChild(document.createTextNode("Agregar Compras"));
+body.appendChild(boton);
+boton.addEventListener("click", () => {
+  elemento = document.createElement("li");
+  texto = document.createTextNode(nuevaTarea.value);
+  elemento.appendChild(texto);
+  lista.appendChild(elemento);
+  document.getElementsByTagName("input")[0].value = "";
+  console.log(texto);
+});
+//  vemos lo nuevo del cliente   //
+boton = document.createElement("button");
+boton.appendChild(document.createTextNode("Sumar Compras"));
+body.appendChild(boton);
+boton.addEventListener("click", () => {
+  elemento = document.createElement("li");
+  texto = document.createTextNode(nuevaTarea.value);
+  elemento.appendChild(texto);
+  lista.appendChild(elemento);
+  document.getElementsByTagName("input")[0].value = "";
+  console.log(texto);
+});
+
+let contenedor = document.createElement("div");
+contenedor.classList.add(".contenedor");
+caja.classList.add(".cuadro");
+body.appendChild(contenedor);
